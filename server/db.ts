17a8f6b4b505254a4a2df0ff184924a401272e49
Wu Peilin <peilinwu@Wus-MacBook-Air.local>
2025-05-20 22:38:39 -0700

    Fix small bug

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import pg from 'pg';
 import * as schema from '@shared/schema';
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL must be set. Did you forget to create a .env file?");
 }
 
-export const pool = new Pool({
+export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
